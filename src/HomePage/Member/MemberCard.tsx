15667bf0dd2ad4ca1b8dcfa7b96f39dc870221c0
Haur514 <haruka.yoshioka.514@gmail.com>
2022-11-03 00:40:43 +0900

    メンバーカードのデザインを変更しました．

--- src/HomePage/Member/MemberCard.tsx
+++ src/HomePage/Member/MemberCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './MemberCard.css';
 import Button from "./../../component/Button";
+import HarukaIcon from "./../../image/userimg/haruka-icon.png"
 
 function MemberCard(props){
     return(
@@ -12,7 +13,12 @@ function MemberCard(props){
             onClick={() => props.setValue(props.name)}
             radius = "0%"
             fontColor={props.selected?"#FFC039":"#121258"}
-            children = {props.name}
+            children = {
+                <div className="MemberCard"> 
+                    <img src={HarukaIcon}/>
+                    {props.name}
+                </div>
+            }
             fontSize = "50px"
         />
     );
