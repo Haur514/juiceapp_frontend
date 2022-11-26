63fd9f2e9068075a0e2e7748bbe151a81d36be53
Haur514 <haruka.yoshioka.514@gmail.com>
2022-11-26 16:30:52 +0900

    #31修正

--- src/HomePage/Item/PopUpMenu/PopUpMenu.tsx
+++ src/HomePage/Item/PopUpMenu/PopUpMenu.tsx
@@ -23,39 +23,41 @@ function PopUpMenu(props){
     }
 
     return(
-        <div className={`popup-menu ${props.visible ? 'visible':'hidden'}`}>
-            <div className="buying">購入</div>
-            <div className="item-info">
-                <img src={props.imgSrc} className="menu-icon"/>
-                <div>{props.name}</div>
-            </div>
-            <div className="select-button-pane">
-                <Button
-                    border="solid 1px black"
-                    color="gray"
-                    height = "2em"
-                    width = "40%"
-                    onClick={() => closePopUp()}
-                    radius = "0.2em"
-                    fontColor="white"
-                    children = {
-                        <div>キャンセル</div>
-                    }
-                    fontSize = "0.7em"
-                />
-                <Button
-                    border="solid 1px black"
-                    color="darkred"
-                    height = "2em"
-                    width = "40%"
-                    onClick={() => purchaseItem()}
-                    radius = "0.2em"
-                    fontColor="white"
-                    children = {
-                        <div>購入</div>
-                    }
-                    fontSize = "0.7em"
-                />
+        <div className={`popup-menu-background ${props.visible ? 'visible':'hidden'}`}>
+            <div className={`popup-menu ${props.visible ? 'visible':'hidden'}`}>
+                <div className="buying">購入</div>
+                <div className="item-info">
+                    <img src={props.imgSrc} className="menu-icon"/>
+                    <div>{props.name}</div>
+                </div>
+                <div className="select-button-pane">
+                    <Button
+                        border="solid 1px black"
+                        color="gray"
+                        height = "2em"
+                        width = "40%"
+                        onClick={() => closePopUp()}
+                        radius = "0.2em"
+                        fontColor="white"
+                        children = {
+                            <div>キャンセル</div>
+                        }
+                        fontSize = "0.7em"
+                    />
+                    <Button
+                        border="solid 1px black"
+                        color="darkred"
+                        height = "2em"
+                        width = "40%"
+                        onClick={() => purchaseItem()}
+                        radius = "0.2em"
+                        fontColor="white"
+                        children = {
+                            <div>購入</div>
+                        }
+                        fontSize = "0.7em"
+                    />
+                </div>
             </div>
         </div>
     );
