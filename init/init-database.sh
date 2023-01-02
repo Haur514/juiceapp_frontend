ccb9675139c7b3fca3f6b2693f295a6882243679
Haur514 <haruka.yoshioka.514@gmail.com>
2023-01-02 19:10:01 +0900

    initスクリプトを更新

--- init/init-database.sh
+++ init/init-database.sh
@@ -1,16 +1,32 @@
 # デモ用のユーザを追加
-curl http://localhost/backend/member/add?name=hoge\&displayName=hoge\&attribute=m1
-curl http://localhost/backend/member/add?name=hoge1\&displayName=hoge1\&attribute=m1
-curl http://localhost/backend/member/add?name=piyo\&displayName=piyo\&attribute=b4
-curl http://localhost/backend/member/add?name=huga\&displayName=huga\&attribute=m2
-curl http://localhost/backend/member/add?name=huga\&displayName=huga\&attribute=m2
-
+curl http://localhost/backend/member/add?name=r-takaic\&displayName=Takaichi\&attribute=m2
+curl http://localhost/backend/member/add?name=r-takagi\&displayName=Takagi\&attribute=m2
+curl http://localhost/backend/member/add?name=h-watanb\&displayName=Watanabe\&attribute=m2
+curl http://localhost/backend/member/add?name=shu-hsmt\&displayName=Hashimoto\&attribute=m2
+curl http://localhost/backend/member/add?name=m-tanigt\&displayName=Taniguchi\&attribute=m2
+curl http://localhost/backend/member/add?name=k-kotou\&displayName=Kotou\&attribute=m2
+curl http://localhost/backend/member/add?name=m-iriyam\&displayName=Iriyama\&attribute=m2
+curl http://localhost/backend/member/add?name=h-yosiok\&displayName=Yoshioka\&attribute=m1
+curl http://localhost/backend/member/add?name=t-ishino\&displayName=Ishino\&attribute=m1
+curl http://localhost/backend/member/add?name=tk-iwase\&displayName=Iwase\&attribute=m1
+curl http://localhost/backend/member/add?name=fumy-oda\&displayName=Oda\&attribute=m1
+curl http://localhost/backend/member/add?name=r-kaichi\&displayName=Kaichi\&attribute=m1
+curl http://localhost/backend/member/add?name=h-takesg\&displayName=Takeshige\&attribute=m1
+curl http://localhost/backend/member/add?name=t-ou\&displayName=Wang\&attribute=m1
+curl http://localhost/backend/member/add?name=y-kaimor\&displayName=Kaimori\&attribute=b4
+curl http://localhost/backend/member/add?name=hkr-kubo\&displayName=Kubo\&attribute=b4
+curl http://localhost/backend/member/add?name=w-mabuti\&displayName=Mabuti\&attribute=b4
+curl http://localhost/backend/member/add?name=k-mihara\&displayName=Mihara\&attribute=b4
+curl http://localhost/backend/member/add?name=ryg-wtnb\&displayName=WatanabeRyouga\&attribute=b4
+curl http://localhost/backend/member/add?name=kusumoto\&displayName=Kusumoto\&attribute=teature
+curl http://localhost/backend/member/add?name=shins\&displayName=Matsumoto\&attribute=teature
+curl http://localhost/backend/member/add?name=higo\&displayName=Higo\&attribute=teature
 
 ## デモ用のアイテムを追加
-curl http://localhost/backend/item/add?name=CocaCora\&sellingprice=100\&costprice=80\&grouping=juice
-curl http://localhost/backend/item/add?name=Fanta\&sellingprice=100\&costprice=80\&grouping=juice
+curl http://localhost/backend/item/add?name=CocaCola\&sellingprice=80\&costprice=70\&grouping=juice
+curl http://localhost/backend/item/add?name=Fanta\&sellingprice=80\&costprice=70\&grouping=juice
 curl http://localhost/backend/item/add?name=GogoTea\&sellingprice=100\&costprice=80\&grouping=juice
-curl http://localhost/backend/item/add?name=Water\&sellingprice=100\&costprice=80\&grouping=juice
+curl http://localhost/backend/item/add?name=Water\&sellingprice=100\&costprice=40\&grouping=juice
 curl http://localhost/backend/item/add?name=PotatoChips\&sellingprice=100\&costprice=80\&grouping=food
 curl http://localhost/backend/item/add?name=Dagashi\&sellingprice=100\&costprice=80\&grouping=food
 
