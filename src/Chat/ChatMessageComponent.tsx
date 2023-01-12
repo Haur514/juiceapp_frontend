3aae3cfa6f358f532a4e10febb9119806caed57a
Haur514 <haruka.yoshioka.514@gmail.com>
2023-01-12 12:35:17 +0900

    implement chat delete

--- src/Chat/ChatMessageComponent.tsx
+++ src/Chat/ChatMessageComponent.tsx
@@ -4,6 +4,13 @@ import "./ChatMessageComponent.css";
 import DefaultIcon from "./../image/userimg/defaultimg.png"
 
 
+const fetchChatDelete = async (id) =>{
+    const inputdata = await fetch(`${window.location.protocol}//${window.location.host}${window.location.pathname}backend/chat/delete?id=${id}`, {
+        method: 'GET',
+        mode: 'cors'
+    });
+}
+
 
 function ChatMessageComponent(props){
 
@@ -16,6 +23,16 @@ function ChatMessageComponent(props){
                     <div className="ChatMessageDate">
                         {props.chat.date}
                     </div>
+                    <div 
+                        className="DeleteButton"
+                        onClick={async () => {
+                            await fetchChatDelete(props.chat.id);
+                            let date = new Date();
+                            props.setLastUpdated(date.getFullYear() + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' +('0' + date.getDate()).slice(-2) + ' ' +  ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2) + '.' + date.getMilliseconds())
+
+                        }}>
+                        Delete
+                    </div> 
                     {/* <div className="GoodReactionButton" onClick={() => {
                         
                     }}> */}
