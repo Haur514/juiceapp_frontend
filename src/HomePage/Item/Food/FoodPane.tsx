6a7ae5f01316e15131d4de970323ca252d0e7ac6
Haur514 <haruka.yoshioka.514@gmail.com>
2023-01-19 20:02:45 +0900

    商品が購入できなかった問題を解決

--- src/HomePage/Item/Food/FoodPane.tsx
+++ src/HomePage/Item/Food/FoodPane.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import './FoodPane.css';
 import ItemCard from "../../../component/ItemCard";
 import LogoCocaCora from "../../../image/logo_coca_cora.jpeg";
+import { a } from "react-spring";
 
 function FoodPane(props){
     return(
         <div className="FoodPane">
             <div className="CategoryName">Food</div>
             <div className="ItemPane-Content FoodPane-flex">
-            {props.foodList.map((food) => {
+            {props.foodList.sort((a,b) => {return - a.salesFigure + b.salesFigure;}).map((food) => {
                 return(
                 <ItemCard
                     color={props.selected?"#121258":"#FFC039"}
