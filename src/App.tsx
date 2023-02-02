9ae4b4809f97d8062646ba219f8059c218c605f2
Haur514 <haruka.yoshioka.514@gmail.com>
2023-02-02 13:20:14 +0900

    アイコンつけて可愛くしました

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,62 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useRef } from 'react';
-import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-import AdminPane from './Admin/AdminPane';
-import './App.css';
-import ChatPane from './Chat/ChatPane';
-import Chat from './Chat/ChatPane';
-import { useGetElementProperty } from './customhook/useGetElementProperty';
-import SellingBarGraph from './Graph/selling/SellingBarGraph';
-import HomePageParent from './HomePage/HomePageParent';
-import HistoryFetch from './util/fetch/HistoryFetch';
-import { Toggle } from './component/Toggle';
-import GraphPane from './Graph/GraphPane';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useRef } from "react";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import AdminPane from "./Admin/AdminPane";
+import "./App.css";
+import ChatPane from "./Chat/ChatPane";
+import Chat from "./Chat/ChatPane";
+import { useGetElementProperty } from "./customhook/useGetElementProperty";
+import SellingBarGraph from "./Graph/selling/SellingBarGraph";
+import HomePageParent from "./HomePage/HomePageParent";
+import HistoryFetch from "./util/fetch/HistoryFetch";
+import { Toggle } from "./component/Toggle";
+import GraphPane from "./Graph/GraphPane";
 
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChartGantt,
+  faHammer,
+  faHouse,
+  faMessage,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const logState = (state) => {
+    console.log("Toggled:", state);
+  };
 
-const logState = state => {
-    console.log("Toggled:", state)
-}
+  const iconStyle: React.CSSProperties = { marginLeft: "0.4em" };
 
   return (
     <Tabs>
-        
-        <TabList>
-            <Tab>Home</Tab>
-            <Tab>Graph</Tab>
-            <Tab>Admin</Tab>
-            <Tab>Chat</Tab>
-        </TabList>
-        <TabPanel>
-            <HomePageParent/>
-        </TabPanel>
-        <TabPanel>
-            <GraphPane/>
-        </TabPanel>
-        <TabPanel>
-            <AdminPane />
-        </TabPanel>
-        <TabPanel>
-            <ChatPane/>
-        </TabPanel>
+      <TabList>
+        <Tab>
+          Home
+          <FontAwesomeIcon style={iconStyle} icon={faHouse} />
+        </Tab>
+        <Tab>
+          Graph
+          <FontAwesomeIcon style={iconStyle} icon={faChartGantt} />
+        </Tab>
+        <Tab>
+          Admin
+          <FontAwesomeIcon style={iconStyle} icon={faHammer} />
+        </Tab>
+        <Tab>Chat<FontAwesomeIcon style={iconStyle} icon={faMessage} /></Tab>
+      </TabList>
+      <TabPanel>
+        <HomePageParent />
+      </TabPanel>
+      <TabPanel>
+        <GraphPane />
+      </TabPanel>
+      <TabPanel>
+        <AdminPane />
+      </TabPanel>
+      <TabPanel>
+        <ChatPane />
+      </TabPanel>
     </Tabs>
   );
 }
--- src/Graph/GraphPane.tsx
+++ src/Graph/GraphPane.tsx
@@ -1,34 +1,38 @@
-
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useRef } from 'react';
-import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-import './GraphPane.css';
-import SellingBarGraph from './selling/SellingBarGraph';
-import LineChart from './linechart/LineChart';
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useRef } from "react";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import "./GraphPane.css";
+import SellingBarGraph from "./selling/SellingBarGraph";
+import LineChart from "./linechart/LineChart";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChartLine, faChartSimple } from "@fortawesome/free-solid-svg-icons";
 
 function GraphPane() {
+  const logState = (state) => {
+    console.log("Toggled:", state);
+  };
 
-const logState = state => {
-    console.log("Toggled:", state)
-}
+  const iconStyle: React.CSSProperties = { marginLeft: "0.4em" };
 
   return (
     <Tabs>
-        
-        <TabList>
-            <Tab>Selling Bar</Tab>
-            <Tab>Line Chart</Tab>
-        </TabList>
-        <TabPanel>
-            <SellingBarGraph/>
-        </TabPanel>
-        <TabPanel>
-            <LineChart/>
-        </TabPanel>
+      <TabList>
+        <Tab>
+          Hot Selling
+          <FontAwesomeIcon style={iconStyle} icon={faChartSimple} />
+        </Tab>
+        <Tab>
+          Sales
+          <FontAwesomeIcon style={iconStyle} icon={faChartLine} />
+        </Tab>
+      </TabList>
+      <TabPanel>
+        <SellingBarGraph />
+      </TabPanel>
+      <TabPanel>
+        <LineChart />
+      </TabPanel>
     </Tabs>
   );
 }
